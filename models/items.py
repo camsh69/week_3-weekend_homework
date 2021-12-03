2e20748bbc6b0d35d5eb8cc6b4ebc2f37d5a1fe6
Campbell Forsyth <camsh69@gmail.com>
2021-12-03 11:59:30 +0000

    models setup commit

---
+++ models/items.py
@@ -0,0 +1,7 @@
+class Items:
+
+    def __init__(self, name_of_item, price, quantity, bought):
+        self.name_of_item = name_of_item
+        self.price = price
+        self.quantity = quantity
+        self.bought = bought
---
+++ models/items_list.py
@@ -0,0 +1,6 @@
+from items import Items
+
+item1 = Items("Milk", 1.00, 1, True)
+item2 = Items("Cheese", 2.25, 1, False)
+
+items = [item1, item2]
