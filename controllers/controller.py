c220505653aca41637098e58b18acf94371caf4c
Campbell Forsyth <camsh69@gmail.com>
2021-12-03 16:11:02 +0000

    Total cost and items extension commit

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,16 +1,18 @@
 from app import app
 from flask import render_template, request, redirect
-from models.items_list import *
+from models.items_list import items, add_new_item, total_price, total_items
 from models.items import Items
 
 @app.route('/shoppinglist')
 def index():
-    return render_template('index.html', title="Shopping List", items=items)
+    return render_template('index.html', title="Shopping List", items=items, 
+    total_price=total_price(items), total_items=(total_items(items)))
 
 @app.route('/shoppinglist', methods=['POST'])
 def add_item():
     name_of_item = request.form['name_of_item']
-    price = float(request.form['price'])
+    input_price = float(request.form['price'])
+    price = "{:.2f}".format(input_price)
     quantity = int(request.form['quantity'])
     bought = True if 'bought' in request.form else False
     new_item = Items(name_of_item, price, quantity, bought)
--- models/items_list.py
+++ models/items_list.py
@@ -1,6 +1,6 @@
 from models.items import *
 
-item1 = Items("Milk", 1.00, 1, True)
+item1 = Items("Milk", 1.25, 1, True)
 item2 = Items("Cheese", 2.25, 1, False)
 
 items = [item1, item2]
@@ -8,4 +8,11 @@ items = [item1, item2]
 def add_new_item(item):
     items.append(item)
 
+def total_price(items):
+    total = 0
+    for item in items:
+        total += item.price
+    return "{:.2f}".format(total)
 
+def total_items(items):
+    return len(items)
