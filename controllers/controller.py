06b4f7b16491f17cdbaccc2bf4a5d6a988968236
Campbell Forsyth <camsh69@gmail.com>
2021-12-05 16:04:54 +0000

    quick refactor

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,7 +6,7 @@ from models.items import Items
 @app.route('/shoppinglist')
 def index():
     return render_template('index.html', title="My Shopping List", items=items, 
-    total_price=total_price(items), total_items=(total_items(items)))
+    total_price=total_price(items), total_items=total_items(items))
 
 @app.route('/shoppinglist', methods=['POST'])
 def add_item():
