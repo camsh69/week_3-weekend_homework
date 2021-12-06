d99716f082f2a79642fbdec6750477c7eb1745db
Campbell Forsyth <camsh69@gmail.com>
2021-12-06 07:45:29 +0000

    Final HW commit with Advanced Extensions

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,10 +3,12 @@ from flask import render_template, request, redirect
 from models.items_list import *
 from models.items import Items
 
+
 @app.route('/shoppinglist')
 def index():
-    return render_template('index.html', title="My Shopping List", items=items, 
-    total_price=total_price(items), total_items=total_items(items))
+    return render_template('index.html', title="My Shopping List", items=items,
+                           total_price=total_price(items), total_items=total_items(items))
+
 
 @app.route('/shoppinglist', methods=['POST'])
 def add_item():
@@ -16,4 +18,18 @@ def add_item():
     bought = True if 'bought' in request.form else False
     new_item = Items(name_of_item, price, quantity, bought)
     add_new_item(new_item)
-    return redirect ('/shoppinglist')
+    return redirect('/shoppinglist')
+
+
+@app.route('/shoppinglist/delete/', methods=['POST'])
+def remove_item_from_list():
+    name_of_item = request.form['name_of_item']
+    remove_item(name_of_item)
+    return redirect('/shoppinglist')
+
+
+@app.route('/shoppinglist/buy/', methods=['POST'])
+def buy():
+    name_of_item = request.form['name_of_item']
+    buy_item(name_of_item)
+    return redirect('/shoppinglist')
--- models/items_list.py
+++ models/items_list.py
@@ -5,8 +5,10 @@ item2 = Items("Cheese", 2.25, 1, False)
 
 items = [item1, item2]
 
+
 def add_new_item(item):
     items.append(item)
+
 
 def total_price(items):
     total = 0.0
@@ -16,8 +18,21 @@ def total_price(items):
             total -= (item.price/10) * item.quantity
     return "{:.2f}".format(total)
 
+
 def total_items(items):
     total = 0
     for item in items:
         total += item.quantity
     return total
+
+
+def remove_item(name_of_item):
+    for item in items:
+        if item.name_of_item == name_of_item:
+            items.remove(item)
+
+
+def buy_item(name_of_item):
+    for item in items:
+        if item.name_of_item == name_of_item:
+            item.bought = True
